a15a2fa9cf0451261495c3d83964c54716e81d99
Dimitar Tasev <dimtasev@gmail.com>
2018-05-08 16:29:28 +0100

    Added repeat last set button to sets

--- today/urls.py
+++ today/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from today.views.create import create_workout
 from today.views.index import IndexView, HistoryView, DeleteView
 from today.views.complete import complete_workout
-from today.views.sets import add_set, del_set
+from today.views.sets import add_set, del_set, repeat_set
 
 app_name = 'today'
 
@@ -15,5 +15,6 @@ urlpatterns = [
     path('history', HistoryView.as_view(), name="history"),
     path('sets/<int:pk>', add_set, name="add_set"),
     path('sets/delete/<int:pk>', del_set, name="del_set"),
+    path('sets/repeat/<int:pk>', repeat_set, name="repeat_set"),
 
 ]
--- today/views/sets.py
+++ today/views/sets.py
@@ -4,6 +4,19 @@ from django.urls import reverse
 from workout.models import WorkoutExercise, SPECIAL_EXERCISES_JOIN_CHARACTER
 
 
+def repeat_set(request, pk):
+    e = WorkoutExercise.objects.get(id=pk)
+    if request.method == "POST":
+        if e.sets != "":
+            if SPECIAL_EXERCISES_JOIN_CHARACTER in e.sets:
+                last_set = e.sets[:e.sets.rfind(SPECIAL_EXERCISES_JOIN_CHARACTER)]
+            else:
+                last_set = e.sets
+            e.sets += SPECIAL_EXERCISES_JOIN_CHARACTER + last_set
+            e.save()
+    return redirect(reverse('today:add_set', kwargs={"pk": e.id}))
+
+
 def del_set(request, pk):
     e = WorkoutExercise.objects.get(id=pk)
     if request.method == "POST":
@@ -20,16 +33,19 @@ def add_set(request, pk):
     if request.method == "GET":
         return render(request, 'today/sets.html', {"exercise": e})
     elif request.method == "POST":
+
+        # return error for missing KGs
         if request.POST["kgs"] == "":
             return render(request, 'today/sets.html',
                           {"exercise": e, "kgs_error": "KGs not specified", "prev_reps_value": request.POST["reps"]})
+        # return error for missing reps
         elif request.POST["reps"] == "":
             return render(request, 'today/sets.html',
                           {"exercise": e, "reps_error": "Reps not specified", "prev_kgs_value": request.POST["kgs"]})
 
         existing = e.sets
         if existing != "":
-            existing += SPECIAL_EXERCISES_JOIN_CHARACTER + "{}x{}".format(request.POST["kgs"], request.POST["reps"])
+            existing += "{}{}x{}".format(SPECIAL_EXERCISES_JOIN_CHARACTER, request.POST["kgs"], request.POST["reps"])
         else:
             existing = "{}x{}".format(request.POST["kgs"], request.POST["reps"])
         e.sets = existing
