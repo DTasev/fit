0c27b021842d953aa4819644bfc8903b5fa2c9fb
Dimitar Tasev <dimtasev@gmail.com>
2018-05-09 08:46:45 +0100

    Revert queryset changes

--- today/views/index.py
+++ today/views/index.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
-            return Workout.objects.get(user_id=self.request.user, date=datetime.date.today())
+            return Workout.objects.filter(user_id=self.request.user, date=datetime.date.today()).first()
 
 
 class HistoryView(generic.ListView):
