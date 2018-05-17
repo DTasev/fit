5c8f52daf8985993541c6c1251f9b9e70221edda
Dimitar Tasev <dimtasev@gmail.com>
2018-05-17 12:27:21 +0100

    Only allow replacement of exercise if no sets are present, also shown an error message if that fails
    Moved log out dropdown a bit more to the left

--- today/views/index.py
+++ today/views/index.py
@@ -4,11 +4,23 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from workout.models import Workout
+from django.contrib import messages
 
 
 class IndexView(generic.ListView):
     model = Workout
     template_name = 'today/index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        storage = messages.get_messages(self.request)
+        msg = None
+        for message in storage:
+            msg = message
+        if msg:
+            context["sets_error_message"] = msg
+        return context
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
--- today/views/replace.py
+++ today/views/replace.py
@@ -7,6 +7,7 @@ from django.views.generic.base import View
 from django.views.generic.detail import SingleObjectMixin
 
 from workout.models import WorkoutExercise, Exercise
+from django.contrib import messages
 
 
 class ReplaceExercise(SingleObjectMixin, View):
@@ -14,9 +15,12 @@ class ReplaceExercise(SingleObjectMixin, View):
 
     def post(self, request, *args, **kwargs):
         e = self.get_object()
+        if e.sets.count() == 0:
+            new_exercise = random.sample(list(Exercise.objects.filter(muscle_group=e.exercise.muscle_group)), k=1)[0]
 
-        new_exercise = random.sample(list(Exercise.objects.filter(muscle_group=e.exercise.muscle_group)), k=1)[0]
-
-        e.exercise = new_exercise
-        e.save()
+            e.exercise = new_exercise
+            e.save()
+        else:
+            # send forward the message to be read in the index view after the redirect
+            messages.add_message(request, messages.ERROR, 'Cannot replace an exercise that has existing sets.')
         return redirect(reverse('today:index'))
