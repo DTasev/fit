8201c47a00cd8172b8e6e4a5e93170382455c2af
Dimitar Tasev <dimtasev@gmail.com>
2018-05-26 20:56:17 +0100

    Added Exercise to admin

--- workout/admin.py
+++ workout/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Exercise
+
+
 # Register your models here.
+
+class ExerciseAdmin(admin.ModelAdmin):
+    model = Exercise
+
+
+admin.site.register(Exercise, ExerciseAdmin)
